673127ffd6073be7c68dd53e89b1a2bba4a656d1
David <daveswaves.2008@gmail.com>
2023-05-12 11:21:24 +0100

    Using Middleware to share username data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -2,14 +2,25 @@
 import Nav from "./Nav.vue";
 
 export default {
-    components: { Nav },
+  components: { Nav },
+
+  computed: {
+    username() {
+      return this.$page.props.auth.user.username;
+    }
+  }
 };
 </script>
 
 <template>
   <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
-      <h1 class="font-bold text-lg">My App</h1>
+      <div class="flex items-center">
+        <h1 class="font-bold text-lg">My App</h1>
+
+        <p class="text-sm ml-4">{{ username }}</p>
+      </div>
+
       <Nav />
     </header>
     
