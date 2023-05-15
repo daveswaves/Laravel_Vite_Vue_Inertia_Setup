968959f8822b8d80e68b12af72e8e2ab07881025
David <daveswaves.2008@gmail.com>
2023-05-15 10:34:42 +0100

    Updated Users.vue

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -10,6 +10,11 @@ const props = defineProps({ users: Array });
   <h1 class="text-3xl">Users</h1>
 
   <ul>
+    <!--
+      database.db3 needs changing to database.sqlite (database dir) and
+      DB_CONNECTION=mysql needs changing to DB_CONNECTION=sqlite (.env)
+      for this to work.
+    -->
     <li v-for="user in users" :key="user.id" v-text="user.name"></li>
   </ul>
 </template>
